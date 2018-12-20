c58a452d3496eadda3349d24895c9a239c37f30b
draptis <dimitrisraptis96@gmail.com>
2018-12-20 22:50:12 +0200

    proof of concept done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import * as eva from 'eva-icons';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    eva.replace();
+  }
+  
   render() {
+    const icons = Object.keys(eva.icons);
+    
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            react-eva-icons
           </p>
           <a
             className="App-link"
@@ -19,6 +26,34 @@ class App extends Component {
           >
             Learn React
           </a>
+            <div style={{
+              padding: 0,
+              margin: 0,
+              listStyle: 'none',
+              display: 'flex',
+              flexFlow: 'row wrap',
+              justifyContent: 'space-around',
+            }}>
+            {icons.map( (icon, index) => (
+              <div style={{
+                background: 'tomato',
+                padding: '5px',
+                width: '200px',
+                height: '150px',
+                marginTop: '10px',
+                
+                lineHeight: '150px',
+                color: 'white',
+                fontWeight: 'bold',
+                fontSize: '3em',
+                textAlign: 'center',
+              }}>
+                
+              <i data-eva={icon} key={index} data-eva-fill="#fff"> </i>
+              </div>
+            ))}
+            </div>
+
         </header>
       </div>
     );
